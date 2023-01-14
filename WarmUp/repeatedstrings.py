ff8ee7b54fef60ae60abf1aacf8aa041c06b2a2d
bentomczyk <bentomczyk@gmail.com>
2023-01-14 15:05:15 +0000

    repeated strings problem

---
+++ WarmUp/repeatedstrings.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'repeatedString' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. LONG_INTEGER n
+#
+
+
+def repeatedString(s, n):
+    string_length = len(s)
+    a_num = s.count("a")
+    remainder = n % string_length
+    remainder_a = s[:remainder].count("a")
+    return ((n//string_length) * a_num) + remainder_a
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input().strip())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
